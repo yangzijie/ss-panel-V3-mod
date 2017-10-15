5e1e23f3f55b3b48dafa37f40248bdc6cf95a266
max2max <2206460474@qq.com>
2017-10-15 15:22:10 +0800

    Update Analytics.php

--- app/Services/Analytics.php
+++ app/Services/Analytics.php
@@ -27,7 +27,7 @@ class Analytics
     public function getShop1User()
     {
         $endtime=Bought::('datetime')+30*86400;
-        if ( $endtime, '>', time() ) {
+        if ( 1, '<', time() ) {
             return Bought::where('shopid', '=', 1)->count();
         }
     }
