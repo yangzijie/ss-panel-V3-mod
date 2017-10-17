820fbe9d88e541c9b39170cb8139551c513a4937
max2max <2206460474@qq.com>
2017-10-17 21:37:30 +0800

    Update Job.php

--- app/Command/Job.php
+++ app/Command/Job.php
@@ -128,7 +128,7 @@ class Job
     {
         $nodes = Node::all();
         foreach ($nodes as $node) {
-            $node->node_last_day_t=$node->bandwidth;
+            $node->node_last_day_t=$node->node_bandwidth;
             $node->save();
         }
         
